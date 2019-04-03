8ee901e870428a75d155e02a18a9e1f1b06214aa
Paolo Barucca <baruccap@gmail.com>
2019-04-03 17:26:33 +0100

    Delete full_degree_seq.m

--- full_degree_seq.m
+++
@@ -1,8 +0,0 @@
-function d_full = full_degree_seq(cin, cout, alpha, nnodes)
-
-d_full = zeros([nnodes 1]);
-
-for i = 1:nnodes
-    d_full(1:nnodes/2) = degree_seq(cin, alpha, nnodes/2);
-    d_full((nnodes/2)+1:nnodes) = degree_seq(cout, alpha, nnodes/2);
-end
