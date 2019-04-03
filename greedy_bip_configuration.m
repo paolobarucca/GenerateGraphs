abfcd787d921ae51f245bd8e2e9494fb7dd492fc
Paolo Barucca <baruccap@gmail.com>
2019-04-03 17:31:29 +0100

    Update greedy_bip_configuration.m

--- greedy_bip_configuration.m
+++ greedy_bip_configuration.m
@@ -1,4 +1,6 @@
 function [A, success] = greedy_bip_configuration(degrees1, degrees2, trials)
+%for success, degrees1 and degrees2 should satisfy the condition that sum(degrees1)==sum(degrees2)
+
 
 success = 0;
 for t = 1:trials
